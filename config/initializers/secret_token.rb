000bc79fd483f7470ffa8e3117155c3ea73c9860
Irene Yiu <apprentice@dbc17.local>
2014-03-20 14:26:22 -0700

    Add initial skeleton for BetterBet app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Betterbet::Application.config.secret_token = 'f71f1412f468d03519c222cf748468ed267a2e3199b3bc50f0c832071f4f27179d49ab34973c1199351826fff8ba9041850fd0f578a8da000cfe37f6f702a560'
