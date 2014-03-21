41ae70aecea0074d3096f29388b78e4174dbdee0
Anne Willborn & Irene Yiu <pair+awillborn.ireneyiu@devbootcamp.com>
2014-03-20 23:47:55 -0700

    Add initial controller test for goal update route

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -5,6 +5,9 @@ class GoalsController < ApplicationController
     @goal = Goal.find(params[:id])
   end
 
+  def update
+  end
+
   def destroy
     @goal.destroy
     redirect_to goals_path
--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe GoalsController do
   let!(:goal) { create :goal }
+  let(:update_goal) { double("goal", title: "My title", description: "My desc", bet_amount: 50) }
   let(:goals) { create_list(:goal, 3) }
   let(:attribs) { attributes_for :goal }
 
@@ -17,14 +18,23 @@ describe GoalsController do
     end
   end
 
+  context '#update' do
+    context 'with valid attributes' do
+      it 'should redirect' do
+        post :update, id: goal: update_goal
+        expect(response).to be_redirect
+      end
+    end
+  end
+
   context '#destroy' do
     it 'should be a redirect' do
-      delete :destroy, id: goal.id
+      delete :destroy, id: goal
       expect(response).to be_redirect
     end
     it 'should delete a goal' do
       expect {
-        delete :destroy, id: goal.id
+        delete :destroy, id: goal
       }.to change { Goal.count }.by(-1)
     end
   end
