0ba871b64cb99b8accd5cc5c24b249559451ed24
thefenry <thefenry@gmail.com>
2014-03-20 21:21:58 -0700

    Change environment to assign keys from facebook yml

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Betterbet::Application.initialize!
+
+APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+APP_NAME = APP_ROOT.basename.to_s
+
+env_config = YAML.load_file(APP_ROOT.join('config', 'facebook.yml'))
+
+  env_config.each do |key,value|
+    ENV[key] = value
+  end
