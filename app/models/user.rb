bb24380755041aa6791660fb0360833ee08f638e
thefenry <thefenry@gmail.com>
2014-03-20 21:24:47 -0700

    Add session save from facebook api

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,16 @@ class User < ActiveRecord::Base
   def all_friends
     self.friends + self.inverse_friends
   end
+
+  def self.omniauth(auth)
+      where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+        user.uid = auth.uid
+        user.name = auth.info.name
+        user.email = auth.info.email
+        user.image = auth.info.image
+        user.token = auth.credentials.token
+        user.expires_at = Time.at(auth.credentials.expires_at)
+        user.save!
+    end
+  end
 end
