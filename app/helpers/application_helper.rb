a3f4d1e2cb18a75c1df44926e0582ffaf4b992bc
Anne Willborn <awillbor@Anne-Willborns-MacBook-Pro.local>
2014-03-20 21:17:12 -0700

    Move user helper methods to app/helpers/application_helper temporarily.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def current_user
+    if session[:id]
+      @current_user ||= User.find(session[:id])
+    end
+  end
+
+  def logged_in?
+    !!session[:id]
+  end
+
 end
