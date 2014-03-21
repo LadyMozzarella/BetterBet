cc9b6b44d7f421435b1301a00678935fb0ca7f5f
thefenry <thefenry@gmail.com>
2014-03-20 21:18:37 -0700

    Add omniauth gem and rb file for facebook adapter

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+  :scope => 'email', :display => 'popup'
+end
