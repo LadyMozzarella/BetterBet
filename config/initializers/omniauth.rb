372fae78b08c283a2acf61075b18f1662bb0aa94
thefenry <thefenry@gmail.com>
2014-03-20 23:37:38 -0700

    Remove spaces and unessesary tabs Make functional yaml to hold oauth keys. The routes work now and all test work

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
   :scope => 'email', :display => 'popup'
 end
