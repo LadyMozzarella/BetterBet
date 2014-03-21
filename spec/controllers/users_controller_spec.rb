712ac8d1f3124382d39c6504a45c039612799c05
Anne Willborn & Irene Yiu <pair+awillborn.ireneyiu@devbootcamp.com>
2014-03-20 17:57:58 -0700

    Add users controller tests for show route Use factory girl to create user and list of goals Add route for users#show Assign variables in users#show route

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,1 +1,22 @@
 require 'spec_helper'
+
+describe UsersController do
+  let(:user) { create :user }
+  let!(:goals) { create_list(:goal, 3) }
+  context '#show' do
+    it 'should be a success' do
+      get :show, id: user.id
+      expect(response).to be_success
+    end
+
+    it 'should assign @user to user' do
+      get :show, id: user.id
+      expect(assigns(:user)).to eq user
+    end
+
+    it 'should assign @goals to goal' do
+      get :show, id: user.id
+      expect(assigns(:goals)).to eq user.goals
+    end
+  end
+end
