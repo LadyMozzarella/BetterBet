e64613e56e04c00bbe61bfffd8bd0c631e80618a
Anne Willborn & Irene Yiu <pair+awillborn.ireneyiu@devbootcamp.com>
2014-03-21 22:37:14 -0700

    Add initial logic for creating a new charge

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,10 +4,9 @@ env_config.each do |key,value|
   ENV[key] = value
 end
 
-
-# Rails.configuration.stripe = {
-#   :publishable_key => ENV['PUBLISHABLE_KEY'],
-#   :secret_key => ENV['SECRET_KEY']
-# }
+Rails.configuration.stripe = {
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key => ENV['SECRET_KEY']
+}
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
