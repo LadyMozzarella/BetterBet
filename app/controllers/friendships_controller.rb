4f38115de00a20e12acd6650aba8aa77c92b38b9
Ivan Sued <apprentice@dbc18.local>
2014-03-22 15:48:19 -0700

    Add call to mailer controller method to deliver mail

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,7 @@
 class FriendshipsController < ApplicationController
   def create
     if current_user.friends << User.find(params[:format])
+      UserMailer.new_friend(current_user, User.find(params[:format])).deliver
       flash[:notice] = "Added friend"
       redirect_to users_path
     else
