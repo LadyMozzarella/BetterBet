dd7a92f2bbcd84f20581ffb60c144478b8695754
Ivan Sued <apprentice@dbc18.local>
2014-03-22 11:25:20 -0700

    Uncomment seed data but do not run if you do not want to already create friendships. Or comment what you do not need

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,22 +4,22 @@ jose = User.create(email: "jose@betterbet.com", password: "jose", password_confi
 irene = User.create(email: "irene@betterbet.com", password: "irene", password_confirmation: "irene", name: "Irene", bio: Faker::Lorem.paragraph)
 anne = User.create(email: "anne@betterbet.com", password: "anne", password_confirmation: "anne", name: "Anne", bio: Faker::Lorem.paragraph)
 
-# ivan.friends << anne
-# ivan.friends << brittany
-# brittany.friends << jose
-# irene.friends << jose
-# irene.friends << anne
+ivan.friends << anne
+ivan.friends << brittany
+brittany.friends << jose
+irene.friends << jose
+irene.friends << anne
 
-# ivan.goals.create(title: "Finish",
-#                   description: "I would like to finish this on time",
-#                   bet_amount: 30,
-#                   start_date: Time.now,
-#                   end_date: Time.now + 20000,
-#                   buddy_id: ivan.friends.find_by_name("Brittany").id)
+ivan.goals.create(title: "Finish",
+                  description: "I would like to finish this on time",
+                  bet_amount: 30,
+                  start_date: Time.now,
+                  end_date: Time.now + 20000,
+                  buddy_id: ivan.friends.find_by_name("Brittany").id)
 
-# irene.goals.create(title: "Be successful",
-#                   description: "I will graduated dbc",
-#                   bet_amount: 100,
-#                   start_date: Time.now,
-#                   end_date: Time.now + 20000,
-#                   buddy_id: irene.friends.find_by_name("jose").id)
+irene.goals.create(title: "Be successful",
+                  description: "I will graduated dbc",
+                  bet_amount: 100,
+                  start_date: Time.now,
+                  end_date: Time.now + 20000,
+                  buddy_id: irene.friends.find_by_name("jose").id)
