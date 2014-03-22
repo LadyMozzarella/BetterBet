0a85deda53d2c245f9eec10df1d1b9ac108d3a83
Anne Willborn <awillbor@Anne-Willborns-MacBook-Pro.local>
2014-03-21 18:32:41 -0700

    add charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -37,5 +37,26 @@ class ChargesController < ApplicationController
       :currency => 'usd',
       :recipient => params[:recipient_id]
       )
+  end
+
+  def create
+
+    p params
+
+  # customer = Stripe::Customer.create(
+  #   email: 'exampleemail',
+  #   card: params[:stripeToken]
+  #   )
+
+  # charge = Stripe::Charge.create(
+  #   customer: customer.id,
+  #   amount: params[:amount],
+  #   currency: 'usd'
+  #   )
+
+  # rescue Stripe::CardError => e
+  #   flash[:error] = e.message
+  #   redirect_to charges_path
+  end
 
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -9,4 +9,11 @@ Rails.configuration.stripe = {
   :secret_key => ENV['SECRET_KEY']
 }
 
+
+# Rails.configuration.stripe = {
+#   :publishable_key => ENV['PUBLISHABLE_KEY'],
+#   :secret_key => ENV['SECRET_KEY']
+# }
+
+
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
