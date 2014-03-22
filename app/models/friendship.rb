1f306a980a89fe226e5dc791ed252b3eb22ea455
Ivan Sued <apprentice@dbc18.local>
2014-03-21 22:39:10 -0700

    remove the attr accessor since we bypass the join table

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,5 +2,4 @@ class Friendship < ActiveRecord::Base
   belongs_to :user, foreign_key: "user_id", class_name: "User"
   belongs_to :friend, foreign_key: "friend_id", class_name: "User"
   validates_presence_of :friend_id, :user_id
-  attr_accessible :friend_id
 end
