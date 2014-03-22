a82c4dca97beca9b78d6a2cbfcfeab1e3f08b687
Brittany Mazza & Ivan Sued <pair+LadyMozzarella.thefenry@devbootcamp.com>
2014-03-21 18:43:47 -0700

    Add seed data. It adds a goal and buddies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ivan = User.create(email: "ivan@betterbet.com", password: "ivan", password_confirmation: "ivan", name: "Ivan", bio: Faker::Lorem.paragraph)
+brittany = User.create(email: "brittany@betterbet.com", password: "Brittany", password_confirmation: "Brittany", name: "Brittany", bio: Faker::Lorem.paragraph)
+jose = User.create(email: "jose@betterbet.com", password: "jose", password_confirmation: "jose", name: "Jose", bio: Faker::Lorem.paragraph)
+irene = User.create(email: "irene@betterbet.com", password: "irene", password_confirmation: "irene", name: "Irene", bio: Faker::Lorem.paragraph)
+anne = User.create(email: "anne@betterbet.com", password: "anne", password_confirmation: "anne", name: "Anne", bio: Faker::Lorem.paragraph)
+
+ivan.friends << anne
+ivan.friends << brittany
+brittany.friends << jose
+irene.friends << jose
+irene.friends << anne
+
+ivan.goals.create(title: "Finish",
+                  description: "I would like to finish this on time",
+                  bet_amount: 30,
+                  start_date: Time.now,
+                  end_date: Time.now + 20000,
+                  buddy_id: ivan.friends.find_by_name("Brittany").id)
