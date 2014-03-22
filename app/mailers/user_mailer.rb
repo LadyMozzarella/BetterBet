8db2d49ea4cce0c287c333f69b2bbe1a9b772bdd
Ivan Sued <apprentice@dbc18.local>
2014-03-22 15:50:17 -0700

    Add method that sends an email to friend when they are added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,4 +5,12 @@ class UserMailer < ActionMailer::Base
     @url  = 'http://localhost:3000/login'
     mail(to: @user.email, subject: 'Welcome to BetterBet')
   end
+
+   def new_friend(user, friend)
+    binding.pry
+    @user = user
+    @friend = friend
+    @url  = 'http://localhost:3000/login'
+    mail(to: @friend.email, subject: 'Welcome to BetterBet')
+  end
 end
