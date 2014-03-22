f1f7f1dfcf710a829cfd291518c3e6b8e20c6e4d
Anne Willborn <awillbor@Anne-Willborns-MacBook-Pro.local>
2014-03-21 18:32:41 -0700

    add charges controller

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,13 @@
+env_config = YAML.load_file(APP_ROOT.join( 'stripe.yml'))
+
+env_config.each do |key,value|
+  ENV[key] = value
+end
+
+
+# Rails.configuration.stripe = {
+#   :publishable_key => ENV['PUBLISHABLE_KEY'],
+#   :secret_key => ENV['SECRET_KEY']
+# }
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
