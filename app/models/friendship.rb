732ad6fbccf6e9c99dfa98a3464dacfbc28e4e6a
Ivan Sued <apprentice@dbc18.local>
2014-03-21 22:17:18 -0700

    Add attribute access for friend_id in order to allow active record to save the info

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,4 +2,5 @@ class Friendship < ActiveRecord::Base
   belongs_to :user, foreign_key: "user_id", class_name: "User"
   belongs_to :friend, foreign_key: "friend_id", class_name: "User"
   validates_presence_of :friend_id, :user_id
+  attr_accessible :friend_id
 end
