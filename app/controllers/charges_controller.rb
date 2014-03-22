47d257e4fc6d17b01bf587bae3eb084581c0199f
Anne Willborn & Irene Yiu <pair+awillborn.ireneyiu@devbootcamp.com>
2014-03-21 22:37:14 -0700

    Add initial logic for creating a new charge

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -21,7 +21,7 @@ class ChargesController < ApplicationController
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to charges_path
-  end
+    end
 
     # https://stripe.com/docs/stripe.js#collecting-bank-account-details
     # separate form and route for this
@@ -37,26 +37,7 @@ class ChargesController < ApplicationController
       :currency => 'usd',
       :recipient => params[:recipient_id]
       )
-  end
 
-  def create
-
-    p params
-
-  # customer = Stripe::Customer.create(
-  #   email: 'exampleemail',
-  #   card: params[:stripeToken]
-  #   )
-
-  # charge = Stripe::Charge.create(
-  #   customer: customer.id,
-  #   amount: params[:amount],
-  #   currency: 'usd'
-  #   )
-
-  # rescue Stripe::CardError => e
-  #   flash[:error] = e.message
-  #   redirect_to charges_path
   end
 
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -9,7 +9,6 @@ Rails.configuration.stripe = {
   :secret_key => ENV['SECRET_KEY']
 }
 
-
 # Rails.configuration.stripe = {
 #   :publishable_key => ENV['PUBLISHABLE_KEY'],
 #   :secret_key => ENV['SECRET_KEY']
