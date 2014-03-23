99ddf2dd54bfedd729215ce14e1b1da3f550b367
Ivan Sued <apprentice@dbc18.local>
2014-03-22 19:38:20 -0700

    remove create custom route that is blocking facebook

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Betterbet::Application.routes.draw do
   get '/dashboard', to: 'pages#dashboard'
   get '/stylemaster', to: 'pages#stylemaster'
 
-  get 'auth/:provider/callback', to: 'sessions#create'
+  # get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/:provider/callback', to: 'sessions#facebook_signup'
   get 'logout', to: 'sessions#destroy'
   get 'login', to: 'sessions#new'
@@ -12,4 +12,5 @@ Betterbet::Application.routes.draw do
   resources :goals
 
   get '/search', to: 'users#search', as: 'search'
+  put '/complete', to: 'goals#complete'
 end
