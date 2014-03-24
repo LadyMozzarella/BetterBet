d4694fda5a79267522dcdebd4aa8e6d5fbbc6ac1
Brittany Mazza <bmazza555@gmail.com>
2014-03-24 12:12:43 -0700

    Integration test WIP

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -7,11 +7,11 @@ describe "User", :js => true do
   describe "can create an account" do
     it "by visiting the new user page" do
       visit new_user_path
-      fill_in 'Name', :with => new_user.name
-      fill_in 'Email', :with => new_user.email
+      fill_in 'Name', :with => 'me'
+      fill_in 'Email', :with => 'me@example.com'
       fill_in 'Password', :with => '123'
       fill_in 'Password confirmation', :with => '123'
-      fill_in 'Bio', :with => new_user.bio
+      fill_in 'Bio', :with => 'what'
       fill_in 'Card Number', :with => "4242424242424242"
       fill_in 'Expiration (MM/YYYY)', :with => "09"
       fill_in '/', :with => "2016"
@@ -32,7 +32,7 @@ describe "User", :js => true do
 
     describe "can view all users" do
       it "by visiting the users index page" do
-        visit users_path
+        # visit users_path
       end
     end
 
