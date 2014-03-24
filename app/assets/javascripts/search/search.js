7112616734814a96957c3e9800948737a16837ea
Anne Willborn <awillborn@gmail.com>
2014-03-24 15:33:43 -0700

    Change search form selector in javascript to align with css class names

--- app/assets/javascripts/search/search.js
+++ app/assets/javascripts/search/search.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var selectors = {
     search: '#search',
-    form: '.search_form'
+    form: '.search_friends'
   }
   Search.controller = new Search.Controller(selectors);
   Search.binder = new Search.Binder(Search.controller, selectors);
