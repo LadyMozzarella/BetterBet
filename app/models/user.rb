d0c6557cda653fcfa2fbc5831615e24d51647729
Ivan Sued <apprentice@dbc18.local>
2014-03-23 20:28:05 -0700

    Remove explicit return and added space after equal sign

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,6 +46,16 @@ class User < ActiveRecord::Base
     users.collect{ |c| {"id" => c["id"], "name" => c["term"], "email" => c["data"]["email"] } }
   end
 
+  def friend_goals
+    friends_info = {}
+    self.friends.each do |friend|
+      unless friend.goals.empty?
+        friends_info[friend.name] = friend.goals.order("updated_at DESC")
+      end
+    end
+    friends_info
+  end
+
   private
 
   def add_to_soulmate
@@ -58,13 +68,4 @@ class User < ActiveRecord::Base
     loader.remove("id" => self.id)
   end
 
-  def friend_goals
-    friends_info = {}
-    self.friends.each do |friend|
-      unless friend.goals.empty?
-        friends_info[friend.name] =friend.goals.order("updated_at DESC")
-      end
-    end
-    return friends_info
-  end
 end
