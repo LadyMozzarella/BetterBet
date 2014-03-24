801e56329f64c6cce66f174935364ea498302134
Brittany Mazza <bmazza555@gmail.com>
2014-03-24 12:36:46 -0700

    Add feature sign up and log in tests

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -4,30 +4,41 @@ describe "User", :js => true do
   let!(:user) { create :user }
   let(:new_user) { create :user }
 
-  describe "can create an account" do
-    it "by visiting the new user page" do
-      visit new_user_path
-      fill_in 'Name', :with => 'me'
-      fill_in 'Email', :with => 'me@example.com'
-      fill_in 'Password', :with => '123'
-      fill_in 'Password confirmation', :with => '123'
-      fill_in 'Bio', :with => 'what'
-      fill_in 'Card Number', :with => "4242424242424242"
-      fill_in 'Expiration (MM/YYYY)', :with => "09"
-      fill_in '/', :with => "2016"
-      fill_in 'CVC', :with => "898"
-      click_button 'Create User'
-      expect(page).to have_content 'Ongoing Goals'
+  describe "sign up or log in" do
+    describe "can create an account" do
+      it "by visiting the new user page" do
+        visit new_user_path
+        fill_in 'Name', :with => new_user.name
+        fill_in 'Email', :with => 'me@example.com'
+        fill_in 'Password', :with => '123'
+        fill_in 'Password confirmation', :with => '123'
+        fill_in 'Bio', :with => new_user.bio
+        fill_in 'Card Number', :with => "4242424242424242"
+        fill_in 'Expiration (MM/YYYY)', :with => "09"
+        fill_in '/', :with => "2016"
+        fill_in 'CVC', :with => "898"
+        click_button 'Create User'
+        expect(page).to have_content 'Ongoing Goals'
+      end
+    end
+
+    describe "can log in" do
+      it "can log in" do
+        visit logout_path
+        fill_in 'Email', :with => user.email
+        fill_in 'Password', :with => user.password
+        click_button 'Login'
+        expect(page).to have_content 'Friends Timeline'
+      end
     end
   end
 
   describe "signed in" do
-    describe "can log in" do
-      # visit logout_path
-      # fill_in 'Email', :with => user.email
-      # fill_in 'Password', :with => user.password
-      # click_button 'Login'
-
+    before(:each) do
+      visit login_path
+      fill_in 'Email', :with => user.email
+      fill_in 'Password', :with => user.password
+      click_button 'Login'
     end
 
     describe "can view all users" do
