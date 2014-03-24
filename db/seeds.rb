10d69d3f3d72c3c3985b6309c5ad5a653bfadac9
Apprentice <apprentice@dbc17.local>
2014-03-24 00:22:33 -0700

    Attach goals to actual users. Add more variety to goals.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,30 +18,15 @@ users.each do |user|
 end
 
 today = Time.now
+goals = ["Graduate from DBC", "Run a marathon", "Drink more water", "Save more money", "Work out 3x a week"]
 
 users.each do |user|
   buddies = (1..10).to_a
   buddies.delete(user.id)
-  Goal.create(title: "Graduate from DBC",
-              description: "Become a badass Ruby and JavaScript developer.",
+  user.goals << Goal.create(title: goals.sample,
+              description: "Betcha I can do it!",
               bet_amount: (10..1000).to_a.sample,
               start_date: today,
-              end_date: today + (20000..99999).to_a.sample,
-              buddy_id: buddies.sample)
-
-  Goal.create(title: "Run a marathon",
-              description: "When I finally have more time.",
-              bet_amount: (10..1000).to_a.sample,
-              start_date: today,
-              end_date: today+ (20000..99999).to_a.sample,
+              end_date: today + (200000..999999).to_a.sample,
               buddy_id: buddies.sample)
 end
-
-# 30.times do
-#   Goal.create(title: Faker::Lorem.word,
-#                 description: Faker::Lorem.paragraph,
-#                 bet_amount: (10..1000).to_a.sample,
-#                 start_date: Time.now,
-#                 end_date: Time.now + (20000..99999).to_a.sample,
-#                 buddy_id: (1..10).to_a.sample)
-# end
