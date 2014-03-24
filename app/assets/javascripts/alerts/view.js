2ce4dba672a48ab1f46c08517400f531224373db
thefenry <thefenry@gmail.com>
2014-03-24 14:06:21 -0700

    create some js pages for the goal completion alert.  Not functional yet

---
+++ app/assets/javascripts/alerts/view.js
@@ -0,0 +1,14 @@
+// Alert.View = function(selectors){
+//   this.selectors = selectors
+// }
+
+// Alert.View.prototype = {
+
+//   updateFriendDiv: function(text){
+//     $(this.selectors.friendStatus).html(text);
+//   },
+
+//   friendErrorAlert: function(xhr){
+//     $(this.selectors.friendStatus).html(xhr.responseText);
+//   },
+// }
