21a10d2491eb16e3d40828d549859e99a4735e04
Brittany Mazza <bmazza555@gmail.com>
2014-03-25 11:45:39 -0700

    Remove p statements

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,5 @@ class DashboardController < ApplicationController
   def index
     @friends = current_user.friend_goals
     @goals = current_user.active_goals
-    p current_user
-    p current_user.friend_goals
   end
 end
