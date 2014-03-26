812f79207bb811a9d0be203e60e7305585a5501a
Anne Willborn <awillborn@gmail.com>
2014-03-26 15:47:42 -0700

    Add betterbet.js file whoops - sorry this wasn't included before

---
+++ app/assets/javascripts/betterbet.js
@@ -0,0 +1,9 @@
+BetterBet = {}
+
+BetterBet.BankAccount = {}
+BetterBet.Alert = {}
+BetterBet.Confirmation = {}
+BetterBet.CreditCard = {}
+BetterBet.Friends = {}
+BetterBet.Goals = {}
+BetterBet.Search = {}
