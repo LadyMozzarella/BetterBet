e53200e827e2b6b6376df7e4a3527c6126008281
Anne Willborn <awillborn@gmail.com>
2014-03-25 20:03:53 -0700

    Recipients controller tests add tests for recipients#new and recipients#create add authorize before_filter to recipients controller

--- spec/controllers/recipients_controller_spec.rb
+++ spec/controllers/recipients_controller_spec.rb
@@ -1,19 +1,30 @@
 require 'spec_helper'
 
 describe RecipientsController do
+  let!(:user) { create :user }
+  before(:each) { session[:user_id] = user.id }
+  let(:token) { StripeMock.generate_bank_token(country: 'US', account_number: '000123456789', routing_number: '110000000') }
+  before { StripeMock.start }
+  after { StripeMock.stop }
+
   context '#new' do
-    it 'should be a success'
+    it 'should be a success' do
+      get :new
+      expect(response).to be_ok
+    end
   end
 
   context '#create' do
     context 'with valid attributes' do
-      it 'should create a recipent'
-      it 'should redirect'
+      it 'should assign bank account to user\'s recipient_id' do
+        post :create, :full_name => "Joe Schmo", :stripeToken => token
+        expect(user.reload.recipient_id).to_not be_nil
+      end
+      it 'should redirect' do
+        post :create, :full_name => "Joe Schmo", :stripeToken => token
+        expect(response).to be_redirect
+      end
     end
 
-    context 'with invalid attributes' do
-      it 'shouln\'t create a recipent'
-      it 'should render'
-    end
   end
 end
