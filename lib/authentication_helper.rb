2bd755eae094a08b44d2b2c782bbefb48bbddecc
Anne Willborn <awillborn@gmail.com>
2014-03-26 14:21:05 -0700

    remove extra indent

--- lib/authentication_helper.rb
+++ lib/authentication_helper.rb
@@ -1,7 +1,7 @@
 module AuthenticationHelper
 
   def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
