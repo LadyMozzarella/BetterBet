33a74e71349cc8d836a0144d78e924d79d4a0af2
Irene Yiu <ireyiu@gmail.com>
2014-03-25 18:34:30 -0700

    Add javascript functionality for the buddy alert. Need extra work to get to correct route

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -55,6 +55,7 @@ class GoalsController < ApplicationController
   end
 
   def confirm
+    binding.pry
     @goal.update_attribute(status_confirmed: params[:complete])
     render :nothing => true, :status => 200
   end
