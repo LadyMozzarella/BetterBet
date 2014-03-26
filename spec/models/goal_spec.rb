950a64b65b30bb9371b743c06c63129f76ebf05d
Brittany Mazza <bmazza555@gmail.com>
2014-03-25 21:46:29 -0700

    WIP

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Goal do
-  describe "validations" do
+  describe 'validations' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :bet_amount }
     it { should validate_presence_of :start_date }
@@ -9,9 +9,37 @@ describe Goal do
     it { should validate_presence_of :owner_id }
   end
 
-  describe "associations" do
+  describe 'associations' do
     it { should belong_to :owner  }
   end
 
+  describe '#duration' do
+    it 'should return the difference between times'
+  end
 
+  describe '#time_remaining' do
+    it 'should return the amount of time left until now'
+  end
+
+  describe '#started?' do
+    describe 'before start date' do
+
+    end
+
+    describe 'after start date' do
+
+    end
+  end
+
+  describe '#countdown' do
+
+  end
+
+  describe '#belongs_to' do
+
+  end
+
+  describe '#expired_goal_by_user' do
+
+  end
 end
