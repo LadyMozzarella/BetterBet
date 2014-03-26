cb83f72fcd9f5cae7cfd3bf09023b3b1643e06d5
Irene Yiu <ireyiu@gmail.com>
2014-03-25 17:10:48 -0700

    Add javascript for the buddy confirmation

--- app/assets/javascripts/confirmations/controller.js
+++ app/assets/javascripts/confirmations/controller.js
@@ -10,16 +10,26 @@ Confirmation.Controller.prototype = {
       dataType: 'json',
       context: this
     }).success(function(response) {
+<<<<<<< HEAD
       if(response.goal !== null ){
         this.goal = response.goal;
         this.view.showModal(this.goal, response.friend);
+=======
+      debugger
+      if(response.length > 0){
+        this.goal = response.goal;
+        this.view.showModal(this.goal);
+>>>>>>> Add javascript for the buddy confirmation
       }
     }).fail(function(xhr){
       console.log(xhr.responseText);
     })
   },
   updateStatus: function(complete) {
+<<<<<<< HEAD
     debugger
+=======
+>>>>>>> Add javascript for the buddy confirmation
     $.ajax({
       url: '/goals/'+ this.goal.id +'/confirm',
       type: 'PUT',
